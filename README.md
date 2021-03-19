eab0b851e2fd9997dbd377ce0adf603f59ce2b1a
lhesnadeemshaikh <nadeem.shaikh@libertyreverse.com>
2021-03-18 21:22:45 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ngfs-basic-types-wtxqaf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/ngfs-basic-types-wtxqaf)
---
+++ index.ts
@@ -0,0 +1,18 @@
+/**
+ * Exercise
+ * 1. Define a new variable of type number
+ * 2. Define a new variable of type string
+ * 3. Try to assign a number value to a string type
+ *    Notice the error
+ * 4. Define a new variable with an infered type
+ * 5. Try to set the value of an infered type to a different type
+ *    Notice the error
+ * 6. Define a new variable of an array of strings
+ * 7. Define a new variable that can be a number or null
+ * 8. Define a new variable that can be a number or a string, 
+ *    then try to set that variable to null
+ *    Notice the error
+ */
+
+// We did #1 for you. You do the rest.
+let myNumber: number;
